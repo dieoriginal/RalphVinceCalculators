a4b6b7b5fbe5fb485a8badd5389c837d1bfb189a
dieoriginal <76889624+dieoriginal@users.noreply.github.com>
2024-10-29 18:18:42 +0100

    back with the Optimal F setled Up

--- components/nav/nav-menu.tsx
+++ components/nav/nav-menu.tsx
@@ -90,6 +90,8 @@ export function NavMenu() {
             </ul>
           </NavigationMenuContent>
         </NavigationMenuItem>
+
+
         <NavigationMenuItem>
           <NavigationMenuTrigger>Algo Stratergies</NavigationMenuTrigger>
           <NavigationMenuContent>
@@ -106,6 +108,25 @@ export function NavMenu() {
             </ul>
           </NavigationMenuContent>
         </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Compound Accounts</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+              {components.map((component) => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
+                >
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+
+
         <NavigationMenuItem>
           <Link href="/docs" legacyBehavior passHref>
             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
